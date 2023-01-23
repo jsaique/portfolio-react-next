6489d242be3c82d69ab952c0a4502ebb2f768b64
jsaique <jsaique11@gmail.com>
2023-01-23 00:04:46 -0500

    Initial commit

---
+++ src/assets/icons.js
@@ -0,0 +1,25 @@
+import avatar1 from "./images/avatar1.png";
+import avatar2 from "./images/avatar2.png";
+import code from "./images/code.png";
+import consulting from "./images/consulting.png";
+import design from "./images/design.png";
+import web1 from "./images/web1.png";
+import web2 from "./images/web2.png";
+import web3 from "./images/web3.png";
+import web4 from "./images/web4.png";
+import web5 from "./images/web5.png";
+import web6 from "./images/web6.png";
+
+export {
+  avatar1,
+  avatar2,
+  code,
+  consulting,
+  design,
+  web1,
+  web2,
+  web3,
+  web4,
+  web5,
+  web6,
+};
